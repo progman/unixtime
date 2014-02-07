158abd6b216ca0e08854c7ad48cc76565d9b96e3
progman <gnuplanet@gmail.com>
2014-02-07 16:18:01 +0400

    Makefile: update run_app()

--- test.sh
+++ test.sh
@@ -8,13 +8,18 @@ APP='./bin/unixtime';
 # run app
 function run_app()
 {
+	local RESULT=0;
+	local STDOUT;
+
 	if [ "${FLAG_VALGRIND}" != "1" ];
 	then
-		STDOUT=$(cat | ${APP} "${@}");
+		STDOUT=$("${APP}" "${@}");
+		RESULT="${?}";
 	else
-		VAL="valgrind --tool=memcheck --leak-check=yes --leak-check=full --show-reachable=yes --log-file=valgrind.log";
+		local VAL="valgrind --tool=memcheck --leak-check=yes --leak-check=full --show-reachable=yes --log-file=valgrind.log";
 
-		STDOUT=$(cat | ${VAL} ${APP} "${@}");
+		STDOUT=$("${VAL}" "${APP}" "${@}");
+		RESULT="${?}";
 
 		echo '--------------------------' >> valgrind.all.log;
 		cat valgrind.log >> valgrind.all.log;
@@ -26,6 +31,8 @@ function run_app()
 	then
 		echo "${STDOUT}";
 	fi
+
+	return "${RESULT}";
 }
 #-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------#
 # test1
